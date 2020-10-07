72da54830a7b24ad32c2b9e7dc857010580a71e7
EdwinKam <peterhi530@gmail.com>
2020-10-06 21:57:16 -0700

    faster way for bracket

--- Bracket/src/com/edwin/Main.java
+++ Bracket/src/com/edwin/Main.java
@@ -1,61 +1,35 @@
 package com.edwin;
 
 import java.util.HashMap;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         Solution s = new Solution();
-        System.out.println(s.isValid("[()"));
+        System.out.println(s.isValid("()"));
     }
 }
+
 class Solution {
-    HashMap<Character,Integer> open= new HashMap<>();
-    HashMap<Character,Integer> close= new HashMap<>();
-
-    Solution(){
-        open.put('{',1);
-        open.put('(',2);
-        open.put('[',3);
-        close.put('}',1);
-        close.put(')',2);
-        close.put(']',3);
-
-    }
-    public boolean isValid(String s) {
-        char[] list=new char[s.length()];
-        int pointer =0;
-        for(int i=0; i<s.length();i++){
-            if(!open.containsKey(s.charAt(i))&&!close.containsKey(s.charAt(i))){
-                System.out.println("1");
-                return false;
+    public boolean isValid(String sg) {
+        Stack<Character> st=new Stack();
+        for(int i=0;i<sg.length();i++){
+            char s=sg.charAt(i);
+            if(s==')'){
+                if(st.empty() || st.pop()!='('){return false;}
             }
-            list[pointer]=s.charAt(i);
-            if(pointer>0&&close.containsKey(s.charAt(i))){
-                if(equals(list[pointer-1],s.charAt(i))){
-                    System.out.println(i);
-                    pointer-=2;
-                }else{
-                    System.out.println("pointer"+pointer);
-                    System.out.println("i"+i);
-                    System.out.println("2");
-                    return false;
-                }
-            }else if(pointer==0&&close.containsKey(s.charAt(i))){
-                System.out.println("3");
-                return false;
-            }else{
-                list[pointer]=s.charAt(i);
+            else if(s==']'){
+                if(st.empty() || st.pop()!='['){return false;}
             }
-            pointer++;
+            else if(s=='}'){
+                if(st.empty() || st.pop()!='{'){return false;}
+            }
+            else{
+                st.push(s);
+            }
         }
-        return (pointer==0);
-    }
-    private boolean equals(char open, char close){
-        if(open=='('&&close==')')return true;
-        if(open=='{'&&close=='}')return true;
-        if(open=='['&&close==']')return true;
-        return false;
+        return st.empty();
     }
 }
